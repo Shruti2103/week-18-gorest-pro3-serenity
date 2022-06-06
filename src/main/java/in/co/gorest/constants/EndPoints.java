fdaf61b702aa568ef4dffac43d634e91279094ba
Shruti2103 <shurtidevani21@gmail.com>
2022-06-06 14:47:52 +0100

    Initial commit

---
+++ src/main/java/in/co/gorest/constants/EndPoints.java
@@ -0,0 +1,27 @@
+package in.co.gorest.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of bestbuy api
+     */
+    public static final String GET_ALL_USERS = "/users";
+    public static final String CREATE_SINGLE_USERS = "/users";
+    public static final String GET_SINGLE_USERS_BY_ID = "users/{usersID}";
+    public static final String UPDATE_USERS_BY_ID = "users/{usersID}";
+    public static final String DELETE_USERS_BY_ID = "users/{usersID}";
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/in/co/gorest/constants/Path.java
@@ -0,0 +1,16 @@
+package in.co.gorest.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for GOREST api
+     */
+    public static final String USERS = "";
+
+
+
+
+
+}
